9f6a26e664af9d481e159f97960b380dd8ca8047
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-17 18:41:00 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,20 @@
+def reverse_each_word(string)
+  string.each do |word|
+end
+puts "#{word}.reverse"
+end
+reverse_each_word("Hello there, and how are you?")
+
+def reverse_each_word(string)
+  string = "Hi again, just making sure it's reversed!"
+  string.each do |word|
+    puts #{word}.reverse
+  end
+end
+
+def reverse_each_word(string)
+string = "Verifying that collect is being called."
+string.collect do |word|
+  puts "#{word}.reverse"
+end
+end
